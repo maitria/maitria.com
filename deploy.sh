48c58e5f7f327e72fa69f1b5c2b3bd28c96012f8
Angela Harms <angela.harms@gmail.com>
2014-09-27 15:51:47 -0400

    aesthetic changes to deploy.sh output

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "==========================================="
+echo
+echo
+echo
+git status
+echo
+
 branch=`git rev-parse --abbrev-ref HEAD`
 if ! [[ "$branch" = "master" ]]
 then
@@ -11,6 +18,8 @@ fi
 echo 'pushing to master'
 git push origin master:master
 echo 'pushed'
+echo
+
 echo 'deploying to maitria webserver'
 rsync -r --progress --delete ./site/ root@104.131.105.47:/srv/maitria.com 
 echo 'deployed'
