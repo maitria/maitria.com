98403a2191bd80ac3bec70fee9746670f4c0ed17
Angela Harms <angela.harms@gmail.com>
2014-12-13 19:49:09 -0500

    Opens deeper URIs & starts and kills server

--- smoketest.sh
+++ smoketest.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "By the way, I'm about to kill any python stuff running."
+killall python
+
 echo "==========================================="
 echo
 echo
@@ -7,8 +10,14 @@ echo "Compiling .sass to .css"
 sass --update site:site
 echo
 
+cd site
+python -m SimpleHTTPServer &
+
+sleep .1
+
+page="/$1"
 echo "Opening page in browser..."
-open -a firefox site/index.html
+open -a firefox http://localhost:8000$page
 echo
 
 git status
@@ -16,4 +25,4 @@ echo
 echo
 echo
 echo
-echo "You should probably commit now."
+echo "If you like what you see on :8000, you should probably commit now."
