214649acf476aa92e792fccff702a9c907dbcbba
Angela Harms <angela.harms@gmail.com>
2014-12-20 15:13:05 -0500

    add clostache as a dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject make-maitria "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :description "app to create static maitria.com site"
+  :url "http://github.com/maitria/maitria.com"
+  :dependencies [[org.clojure/clojure "1.6.0"] 
+                 [de.ubercode.clostache/clostache "1.4.0"]]
   :main ^:skip-aot make-maitria.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
