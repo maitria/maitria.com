6cfae60d2ea5b4530626fd182810eff5d6822cd5
Angela Harms <angela.harms@gmail.com>
2014-12-21 15:58:40 -0500

    extract variables for clarity

--- src/make_maitria/core.clj
+++ src/make_maitria/core.clj
@@ -1,7 +1,6 @@
 (ns make-maitria.core
   (:require [clostache.parser :refer :all]
             [clojure.java.io :as io])
-  
   (:gen-class))
 
 (def pages ["index.html"
@@ -17,8 +16,9 @@
 
 (defn generate
   [page]
-  (let [partials {:header (slurp (io/resource "partials/header.mustache"))}]
-    (spit (destination-file page) (render-resource (source-file page) {} partials))))
+  (let [partials {:header (slurp (io/resource "partials/header.mustache"))}
+        variables {:there-is-currently "no data needed"}]
+    (spit (destination-file page) (render-resource (source-file page) variables partials))))
 
 (defn -main
   [& args]
