47d922ee63073a90abab1db3d1492d08d3c1c568
Angela Harms <angela.harms@gmail.com>
2014-12-21 14:56:46 -0500

    extract (source-file ...)

--- src/make_maitria/core.clj
+++ src/make_maitria/core.clj
@@ -2,7 +2,11 @@
   (:require [clostache.parser :refer :all])
   (:gen-class))
 
+(defn source-file
+  [destination]
+  (str destination ".mustache"))
+
 (defn -main
   [& args]
-  (spit "site/index.html" (render-resource "index.html.mustache" {}))
+  (spit "site/index.html" (render-resource (source-file "index.html") {}))
   (spit "site/events/intro-to-meditation.html" (render-resource "events/intro-to-meditation.html.mustache")))
