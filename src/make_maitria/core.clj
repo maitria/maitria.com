580d60d6b9b59dd505b3832463fb80dfaca91888
Angela Harms <angela.harms@gmail.com>
2015-06-02 14:19:07 -0400

    Move index page events to a separate file

--- src/make_maitria/core.clj
+++ src/make_maitria/core.clj
@@ -16,7 +16,8 @@
 
 (defn generate
   [page]
-  (let [partials {:header (slurp (io/resource "partials/header.mustache"))}
+  (let [partials {:header (slurp (io/resource "partials/header.mustache"))
+                  :events (slurp (io/resource "partials/events.mustache"))}
         variables {:there-is-currently "no data needed"}
         content (render-resource (source-file page) variables partials)]
     (spit (destination-file page) content)))
