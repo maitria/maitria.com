9cc914e56c49a7fbf1497769cb8336fb87d509a0
Angela Harms <angela.harms@gmail.com>
2015-06-24 19:44:14 -0400

    Extract contact to a partial

--- src/make_maitria/core.clj
+++ src/make_maitria/core.clj
@@ -19,7 +19,8 @@
   (let [partials {:header (slurp (io/resource "partials/header.mustache"))
                   :events (slurp (io/resource "partials/events.mustache"))
                   :conference (slurp (io/resource "partials/conference.mustache"))
-                  :about (slurp (io/resource "partials/about.mustache"))}
+                  :about (slurp (io/resource "partials/about.mustache"))
+                  :contact (slurp (io/resource "partials/contact.mustache"))}
         variables {:there-is-currently "no data needed"}
         content (render-resource (source-file page) variables partials)]
     (spit (destination-file page) content)))
