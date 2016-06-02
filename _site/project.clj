96e706a0431f67fb0b24c1d1552de9e6a8ae8ae0
Alex Harms <onealexharms@gmail.com>
2016-06-02 12:17:43 -0400

    Remove _site directory.

--- _site/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject make-maitria "0.1.0-SNAPSHOT"
-  :description "app to create static maitria.com site"
-  :url "http://github.com/maitria/maitria.com"
-  :dependencies [[org.clojure/clojure "1.6.0"] 
-                 [de.ubercode.clostache/clostache "1.4.0"]]
-  :main ^:skip-aot make-maitria.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
